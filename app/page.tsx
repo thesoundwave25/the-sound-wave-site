72be0ff812e2595b01874cb17264e87a1fd12ee0
Wallen <thesoundwave25@gmail.com>
2026-02-13 12:34:30 +0100

    logo gian

--- app/page.tsx
+++ app/page.tsx
@@ -1158,7 +1158,7 @@ Dj Set ed Eventi che fanno vibrare il pubblico</p>
     {/* ✅ Loghi: una riga anche su mobile (come desktop) */}
     <div className="mt-6 flex flex-nowrap justify-center gap-2 sm:gap-6">
       {/* ✅ SOLO QUI: Gianluk sm:w-36 come gli altri */}
-      <div className="relative h-12 w-20 sm:h-16 sm:w-44">
+      <div className="relative h-12 w-20 sm:h-16 sm:w-36">
         <Image
           src="/brand/gianluk-logo.svg"
           alt="logo"
