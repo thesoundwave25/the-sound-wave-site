55b0b4035ee24869d0f96ab28e6e8e444a40ff84
Wallen <thesoundwave25@gmail.com>
2026-02-15 10:48:29 +0100

    fix navbar fissa 3

--- app/layout.tsx
+++ app/layout.tsx
@@ -38,12 +38,11 @@ export default function RootLayout({
         <link rel="manifest" href="/site.webmanifest" />
         {/* --- FINE SEZIONE ICONE --- */}
       </head>
-      <body>
-        <div style={{ position: "relative", zIndex: 1 }}>
-          {children}
-        </div>
-        <Analytics />
-      </body>
+      <body className="min-h-screen overflow-x-clip">
+  {children}
+  <Analytics />
+</body>
+
     </html>
   );
 }
